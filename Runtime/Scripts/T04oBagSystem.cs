b478da584aedcc0e9dc36f3ebadea937cf0175e5
Andrey <cool.dddaa@yandex.ru>
2026-04-06 17:25:33 +0500

    save

---
+++ Runtime/Scripts/T04oBagSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonSharp;
+namespace TETR04o {
+    public class T04oBagSystem : UdonSharpBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
